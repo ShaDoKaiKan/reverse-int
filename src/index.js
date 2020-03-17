d88ae23d24ca8a4d662ed412d5c5c8e90a45cd23
ShaDoKaiKan <sha.alia95@gmail.com>
2020-03-17 10:55:09 +0300

    feat: add solution for reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let rev = n.toString().split('');
+    let arr = Array.from(rev).reverse();
+    let res;
+    if (arr.includes('-')) {
+        arr.pop();
+        res = arr.join('');
+    } else {
+        res = arr.join('');
+    }
+    
+    
+    return res;
 }
